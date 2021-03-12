fd16ccd01a4d83f9c9990def9de1ba93ee49068f
Thiago <thiago.kaique01@gmail.com>
2021-03-11 21:33:42 -0300

    [feature] identation

--- McGyver/Program.cs
+++ McGyver/Program.cs
@@ -21,9 +21,26 @@ namespace McGyver
         }
         void solve()
         {
-            solve(0, data.GetLength(0)) ;
+            solve(0, data.GetLength(0),true) ;
         }
-        void solve(int right,int left)
+        void solve(int right,int left,bool line)
+        {
+            int middle = (right + left) / 2;
+            if (left - right <= 1)
+            {
+                if (!line) Console.Write("           ");
+                Console.Write($" return '{data[middle, 0]}';\n");
+                return;
+            }
+            else if(line)Console.Write("\n     ");
+            Console.Write($"if(m < '{data[middle, 0]}')");
+            solve(right, middle,true);
+            Console.Write("else ");
+            solve(middle, left,false);
+        }
+        /*
+         * implementation of the functional algorithim without identation
+        void solve(int right, int left)
         {
             int middle = (right + left) / 2;
             if (left - right <= 1)
@@ -32,10 +49,10 @@ namespace McGyver
                 return;
             }
             Console.WriteLine($"if(m < '{data[middle, 0]}')");
-            solve(right, middle);
+            solve_basic(right, middle);
             Console.WriteLine("else");
-            solve(middle, left);
-        }
+            solve_basic(middle, left);
+        }*/
     }
 
 }   
